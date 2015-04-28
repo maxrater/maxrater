f91e52d3fecabd6340d08bee1650de2b07e4b294
Max Rater <msrater@gmail.com>
2015-04-28 02:55:47 -0400

    New changes

---
+++ public/js/application.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+
+// window.addEventListener("load", function(){
+//     var load_screen = document.getElementById("load_screen");
+//     document.body.removeChild(load_screen)
+// });
+
+});
